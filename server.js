f261b4f223f59f662e42889f88d9394d10def7e7
Abdulhakeem Dahir <somlutions@gmail.com>
2020-12-20 12:36:48 -0800

    fix readfilesybc

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 const fs = require("fs");
 const { json } = require("express");
-const data = require(__dirname, "./db/db.json");
+const dataPath = require("./db/db.json");
 
 // Tells node that we are creating an "express" server
 var app = express();
@@ -15,26 +15,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/api/notes", function (req, res) {
-  res.header("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "/db/db.json"));
-});
+const readData = fs.readFileSync("./db/db.json", "utf8");
 
-// app.post("/api/notes", function (req, res) {
-//   const newReservation = req.body;
-//   console.log(req.body);
-//   database.push(newReservation);
+let dataParse = JSON.parse(readData);
+console.log(dataParse);
+// app.get("/api/notes", function (req, res, dataPath) {
+//   fs.readFile("/db/db.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       let dataParse = JSON.parse(data);
+//       console.log(dataParse);
+//       return;
+//     }
+//     console.log(data);
+//   });
 
-//   res.json(newReservation);
+//   fs.readFile(dataPath, "utf8", (err, data) => {
+//     if (err) throw err;
+//     let dataParse = JSON.parse(data);
+//     console.log(dataParse);
+//   });
+//   // res.header("Content-Type", "application/json");
+//   // res.sendFile(path.join(__dirname, "/db/db.json"));
 // });
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
+// // app.post("/api/notes", function (req, res) {
+// //   const newReservation = req.body;
+// //   console.log(req.body);
+// //   database.push(newReservation);
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
+// //   res.json(newReservation);
+// // });
+
+// app.get("/notes", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public/notes.html"));
+// });
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
